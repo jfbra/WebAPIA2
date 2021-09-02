caf0862a0b04dea6167fa2f9142d3638d1ed136e
James Brady <jfbrady12345@gmail.com>
2021-09-02 12:11:52 +1200

    Setup completed, just going to try to created databse.

---
+++ Data/WebAPIDBContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebAPIA2.Models;
+
+namespace WebAPIA2.Data
+{
+    public class WebAPIDBContext : DbContext
+    {
+        public WebAPIDBContext(DbContextOptions<WebAPIDBContext> options) : base(options) { }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source = WebAPIDatabase.sqlite");
+        }
+    }
+}
---
+++ Models/Order.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebAPIA2.Models
+{
+    public class Order
+    {
+        [Key]
+        public int Id { get; set; }
+        public string  UserName { get; set; }
+        [Required]
+        public string ProductID { get; set; }
+        [Required]
+        public int Quantity { get; set; }
+    }
+}
