e3755426a0be0f22a36f6b14ad2c10cbad7ed7da
James Brady <jfbrady12345@gmail.com>
2021-09-03 00:43:56 +1200

    Created OrdersController and OrderInputDto in preparation for endpoint 3.

---
+++ Dtos/OrderInputDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebAPIA2.Dtos
+{
+    public class OrderInputDto
+    {
+        [Required]
+        public int ProductID { get; set; }
+        [Required]
+        public int Quantity { get; set; }
+    }
+}
